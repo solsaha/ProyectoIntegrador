9d92c9b5038749ffde2b10378a6c651c648cf956
magacarolina <mparra@udesa.edu.ar>
2021-09-09 09:28:46 -0300

    cambios

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,7 +13,6 @@ export default class Card extends Component {
             <div className = 'album'>
                {<img src={this.props.cover_medium} alt=""/> }
                { <h4> Nombre del album:{this.props.title} </h4> }
-              
                { <h4> Artista: {this.props.artist}</h4> }
                { <h4> Link al album: {this.props.link}</h4> }
                {/* <button onClick={()=> this.props.removerPersonaje(this.props.name)}> Eliminar personaje</button> */}
