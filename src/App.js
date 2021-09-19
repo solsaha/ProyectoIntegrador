746b1ce52533d8e3ac39ef83e72411a27002eb11
nicolesuchmon <nickysuchmon@gmail.com>
2021-09-19 20:03:20 -0300

    css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import imagen from './LogoA.png'
-import NavBar from './components/NavBar/NavBar';
+
 import CancionContainer from "./components/CancionContainer/CancionContainer";
 import Footer from './components/Footer/Footer';
 
@@ -12,7 +12,7 @@ function App() {
       
     <img src={imagen} alt="logo" className= "imglogo"/> 
     
-     <NavBar/>
+    
      <CancionContainer/>
      <Footer/>
      </div>
